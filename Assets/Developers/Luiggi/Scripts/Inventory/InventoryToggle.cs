cb2e855962bc0bc1f71fee605382b2c47de20119
Luiginoo0 <luigiromano417@gmail.com>
2026-04-02 18:55:34 +0200

    freccetta UI inventario

--- Assets/Developers/Luiggi/Scripts/Inventory/InventoryToggle.cs
+++ Assets/Developers/Luiggi/Scripts/Inventory/InventoryToggle.cs
@@ -1,20 +1,32 @@
 using UnityEngine;
+using UnityEngine.InputSystem; // <-- Abbiamo aggiunto questa libreria!
 
 public class InventoryToggle : MonoBehaviour
 {
     public GameObject inventoryPanel;
+    public GameObject customCursor;
 
     private void Start()
     {
-        inventoryPanel.SetActive(false); // chiuso di default
+        inventoryPanel.SetActive(false); 
+        customCursor.SetActive(false);   
+        Cursor.visible = false;          
     }
 
     private void Update()
     {
+        // Controllo del clic con il NUOVO Input System
+        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Cursor.visible = false;
+        }
+
         if (InputManager.ToggleInventory)
         {
             bool isOpen = inventoryPanel.activeSelf;
+            
             inventoryPanel.SetActive(!isOpen);
+            customCursor.SetActive(!isOpen);
         }
     }
 }
