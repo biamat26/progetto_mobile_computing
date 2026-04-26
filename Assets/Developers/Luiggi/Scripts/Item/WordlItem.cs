573a847805037b22cddc2e7169b27822fb764655
Luiginoo0 <luigiromano417@gmail.com>
2026-04-26 18:26:55 +0200

    input system fatto meglio

--- Assets/Developers/Luiggi/Scripts/Item/WordlItem.cs
+++ Assets/Developers/Luiggi/Scripts/Item/WordlItem.cs
@@ -8,37 +8,35 @@ public class WorldItem : MonoBehaviour
     private Transform playerTransform;
     private bool playerNearby = false;
 
-void Start()
-{
-    pickupPrompt = transform.Find("PickUpPrompt")?.gameObject;
-    if (pickupPrompt != null) pickupPrompt.SetActive(false);
-
-    GameObject player = GameObject.FindGameObjectWithTag("Player");
-    if (player != null) playerTransform = player.transform;
-}
-
-void Update()
-{
-    if (playerTransform == null)
+    void Start()
     {
+        pickupPrompt = transform.Find("PickUpPrompt")?.gameObject;
+        if (pickupPrompt != null) pickupPrompt.SetActive(false);
+
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         if (player != null) playerTransform = player.transform;
-        else Debug.Log("Player non trovato!");
-        return;
     }
 
-    float distance = Vector2.Distance(transform.position, playerTransform.position);
-    Debug.Log("Distanza: " + distance + " | pickupRange: " + pickupRange);
-
-    bool inRange = distance <= pickupRange;
-    playerNearby = inRange;
-    if (pickupPrompt != null) pickupPrompt.SetActive(inRange);
-
-    if (playerNearby && Input.GetKeyDown(KeyCode.E))
+    void Update()
     {
-        if (InventorySystem.Instance == null) return;
-        bool picked = InventorySystem.Instance.AddItem(itemData);
-        if (picked) Destroy(gameObject);
+        if (playerTransform == null)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            if (player != null) playerTransform = player.transform;
+            else return;
+        }
+
+        float distance = Vector2.Distance(transform.position, playerTransform.position);
+
+        bool inRange = distance <= pickupRange;
+        playerNearby = inRange;
+        if (pickupPrompt != null) pickupPrompt.SetActive(inRange);
+
+        if (playerNearby && InputManager.Interact)
+        {
+            if (InventorySystem.Instance == null) return;
+            bool picked = InventorySystem.Instance.AddItem(itemData);
+            if (picked) Destroy(gameObject);
+        }
     }
-}
 }
